9c45cb1bf8ded7377e8a29efda174b982ece4bb9
gavin <f106524018@g.ncu.edu.tw>
2017-09-20 00:36:52 +0800

    Source Download

--- DownloadSource.py
+++ DownloadSource.py
@@ -2,20 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import fetch_mldata
-#from chainer import cuda, Variable, FunctionSet, optimizers
-#import chainer.functions  as F
 import sys
 
 plt.style.use('ggplot')
-
-# 確率的勾配降下法で学習させる際の１回分のバッチサイズ
-batchsize = 100
-
-# 学習の繰り返し回数
-n_epoch   = 1
-
-# 中間層の数
-n_units   = 1000
 
 # MNISTの手書き数字データのダウンロード
 # #HOME/scikit_learn_data/mldata/mnist-original.mat にキャッシュされる
@@ -25,8 +14,7 @@ mnist = fetch_mldata('MNIST original')
 # sample img in SourceImg folder
 mnist.data   = mnist.data.astype(np.float32)
 mnist.data  /= 255     # 0-1のデータに変換
-# mnist.target : 正解データ（教師データ）
-mnist.target = mnist.target.astype(np.int32)
+
 
 # 手書き数字データを描画する関数
 def draw_digit(data,i):
@@ -48,12 +36,7 @@ def draw_digit(data,i):
     plt.cla()
     plt.close()
 
-#draw_digit(mnist.data[5],5)
-#draw_digit(mnist.data[12345],12345)
-#draw_digit(mnist.data[33456],34356)
-
-
-#np.savetxt('test.txt', mnist.data[:60000],fmt="%i", delimiter=',')
 r_base = 1
-for idx in range(r_base,r_base+1000):
-    draw_digit(mnist.data[idx],idx)
+for idx in range(r_base,r_base+70000,2): # I just want half of training set
+    draw_digit(mnist.data[idx],idx) 
+    print str(idx)
